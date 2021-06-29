0670e6c01d65d63cef0defd00e712f92f6dd5139
Augusto Ciuffoletti <augusto.ciuffoletti@gmail.com>
2021-06-29 10:50:02 +0200

    fix errore get (append)

--- flask/server.py
+++ flask/server.py
@@ -1,16 +1,44 @@
 from flask import Flask, request, abort
 from redis.client import Redis
-
+import uuid
+import json
+ 
 app = Flask(__name__)
 r = Redis()
-
-@app.route('/value/<key>',methods = ['POST', 'GET'])
+ 
+@app.route('/<key>',methods = ['GET','POST','PUT'])
 def KVsvc(key):
+##########
+# GET
+##########
 	if request.method == 'GET':
-		if ( r.exists(key) ):
-			return r.get(key)
+# get value as a JSON string
+		value = r.get(key)
+		if ( value != None ):
+# decode JSON value
+			try:
+				data = json.loads(value)
+			except Exception as e:
+				print(e)
+				abort(500, "GET: Data error")
+# find new key
+			newKey = uuid.uuid4().hex[0:8]
+			while ( r.exists(newKey) ):
+				newKey = uuid.uuid4().hex[0:8]
+# store value in new key
+			r.set(newKey,value)
+# delete old key
+			r.delete(key)
+# return value and the new key
+			print(value)
+			data[2]=newKey
+			print(data)
+			return json.dumps(data)
 		else:
 			abort(404, "GET: The key does not exists in the database")
+##########
+# POST
+##########
 	elif request.method == 'POST':
 		data=request.get_data()
 		print(data)
@@ -20,7 +48,18 @@ def KVsvc(key):
 			return data
 		else:
 			abort(404, "POST: The key does not exists in the database")
-	return abort(405,"Only GET and POST methods are accepted on this route")
+##########
+# PUT
+##########	
+	elif request.method == 'PUT':
+		data=request.get_data()
+		if ( r.exists(key) ):
+			abort(409, "PUT: The key already exists in the database")
+		else:
+			r.set(key,data)
+			return data
+	else:		
+		return abort(405,"Only GET and POST methods are accepted on this route")
 			
 @app.route('/new/<key>')
 def NewKey(key):
